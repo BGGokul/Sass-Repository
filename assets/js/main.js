5abc653d1754d4779f8949c49895c63e7e0e62b7
BGGokul <103502387+BGGokul@users.noreply.github.com>
2022-05-07 16:42:33 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,41 @@ menuToggler.addEventListener("click", () => {
     icon.classList.toggle("d-none");
   });
 });
+
+(function () {
+  const headerDOM = document.getElementById("header");
+  const navTopOffSet = headerDOM.clientHeight + 100;
+
+  window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (scroll >= navTopOffSet) {
+      headerDOM.classList.add("header-fixed");
+    } else {
+      headerDOM.classList.remove("header-fixed");
+    }
+  });
+})();
+
+function headerMenuActive() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".header--menu__item>a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+    navLinks.forEach((li) => {
+      li.classList.remove("active");
+      if (current == li.getAttribute("href").split("#")[1]) {
+        li.classList.add("active");
+      }
+    });
+  });
+}
+headerMenuActive();
